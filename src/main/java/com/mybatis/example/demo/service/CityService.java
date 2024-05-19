45ec32f5e61de56296072e0ad115ee0a54a52790
noefame <amaranthe@nate.com>
2024-05-20 00:25:35 +0900

    Spring Boot Mybatis Logback Init

--- src/main/java/com/mybatis/example/demo/service/CityService.java
+++ src/main/java/com/mybatis/example/demo/service/CityService.java
@@ -4,6 +4,7 @@ import com.mybatis.example.demo.model.City;
 import com.mybatis.example.demo.repository.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,12 +18,12 @@ public class CityService {
         this.cityRepository = cityRepository;
     }
 
-
+    @Transactional
     public void saveCity(City city) {
 
         cityRepository.saveCity(city);
     }
-
+    @Transactional(readOnly = true)
     public List<City> selectList() {
 
         return cityRepository.selectList();
